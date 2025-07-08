6f0eae85baddd0b06aece89ebd019c3bcf43157c
Marinadssantoss <pereira.santos.marina@escola.pr.gov.br>
2025-07-08 10:15:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'qual é o maior campeão do Brasil?',
+    'O maior campeão do Brasil é o Palmeiras'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como se diz bem vindo na língua espanhola?',
+    'Em espanhol bem vindo se diz bienvenido'
+)
+
+criaCartao(
+    'Moda',
+    'Qual peça de roupa é considerada um clássico atemporal, presente em quase todo guarda-roupa? ',
+    'A calça jeans é amplamente considerada um clássico atemporal da moda, adaptável a diversas ocasiões e estilos.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Inglatera?',
+    'A capital da Inglaterra é Londres'
+)
